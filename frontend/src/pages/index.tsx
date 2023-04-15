59b33f0fba3a39578dedae031b448f5af2bbdd33
Loc Nguyen <vinhloc30796@gmail.com>
2023-04-15 15:59:00 +0800

    Install Tailwind

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
           app="business"
           credentials="elasticsearch:changeme"
         />
-        <h1>Hello World</h1>
+        <h1 className="text-3xl font-bold">Hello World</h1>
       </main>
     </>
   );
