66460dfe386db133dbbb8ce96ab7973fc2ac2a04
Loc Nguyen <vinhloc30796@gmail.com>
2023-05-17 12:53:05 +0800

    Add debouncing to searchbox

---
+++ frontend/src/components/_debounce.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+function useDebounce(value: any, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(
+    () => {
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [value, delay]
+  );
+
+  return debouncedValue;
+}
+
+export { useDebounce };
